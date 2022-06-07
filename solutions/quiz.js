201e4eeb227f218d0a8f418b2ea91f6c7cd9e2f6
Martin Burolla <mburolla@xpanxion.com>
2022-06-07 12:24:34 -0400

    update

---
+++ solutions/quiz.js
@@ -0,0 +1,13 @@
+
+
+// const console.log() vs return what does this function return?
+
+
+const update = async (a, b) => {
+    a = 2;
+    b = "hi";
+}
+let a = 1;
+let b = "test";
+update(a,b);
+console.log(a, b)
