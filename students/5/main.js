094c49f6c3cf4eefdbd6fd0a4a493b8f91bace2b
Diane Tewah <dtewah@xpanxion.com>
2022-06-08 11:43:33 -0400

    Ex1: getUTCDateTime

--- students/5/main.js
+++ students/5/main.js
@@ -6,35 +6,49 @@
 const { default: axios } = require("axios");
 
 const ex1 = async () => {
-    console.log("TODO...")
-}
+  try {
+    let theDate = await getUTCDateTime();
+    console.log(theDate);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const ex2 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 const ex3 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 const ex4 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 const ex5 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 const ex6 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 //
 // Your functions here...
-//
+//exercise 1
+const getUTCDateTime = () => {
+  return new Promise((resolve) => {
+    let utcDateTimeNow = new Date().toUTCString();
+    resolve(utcDateTimeNow);
+  });
+};
+
+
+
 
 const main = async () => {
-    ex1();
-}
+  ex1();
+};
 
 main();
