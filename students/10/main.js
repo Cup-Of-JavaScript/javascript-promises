77df6b9a19d8a315827996fbd77ca12588ae520e
Martin Burolla <mburolla@xpanxion.com>
2022-06-08 10:11:43 -0400

    Add dir

---
+++ students/10/main.js
@@ -0,0 +1,40 @@
+//
+// File: main.js
+// Date: 6/6/2022
+//
+
+const { default: axios } = require("axios");
+
+const ex1 = async () => {
+    console.log("TODO...")
+}
+
+const ex2 = async () => {
+    console.log("TODO...")
+}
+
+const ex3 = async () => {
+    console.log("TODO...")
+}
+
+const ex4 = async () => {
+    console.log("TODO...")
+}
+
+const ex5 = async () => {
+    console.log("TODO...")
+}
+
+const ex6 = async () => {
+    console.log("TODO...")
+}
+
+//
+// Your functions here...
+//
+
+const main = async () => {
+    ex1();
+}
+
+main();
