46a37b6ce442baab403740592107205346ae1bc2
Sherika Fayson <sfayson@xpanxion.com>
2022-06-08 15:11:47 -0400

    Ex2: countCharsInBody

--- students/10/main.js
+++ students/10/main.js
@@ -6,30 +6,30 @@
 const { default: axios } = require("axios");
 
 const ex1 = async () => {
-    let theDate = await getUTCDateTime();
-    console.log(theDate);
-}
+  let theDate = await getUTCDateTime();
+  console.log(theDate);
+};
 
 const ex2 = async () => {
-    let characters = await countCharsInBody(1);
-    console.log(characters)
-}
+  let characters = await countCharsInBody(1);
+  console.log(characters);
+};
 
 const ex3 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 const ex4 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 const ex5 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 const ex6 = async () => {
-    console.log("TODO...")
-}
+  console.log("TODO...");
+};
 
 //
 // Your functions here...
@@ -37,31 +37,29 @@ const ex6 = async () => {
 
 //ex2
 const countCharsInBody = async (userId) => {
-    let count = 0;
-    try {
-    let result = await axios.get(`https://jsonplaceholder.typicode.com/posts/${userId}`);
-        count = result.data.body.length
-    }
-    catch (error) {
-        console.log(error);
-    }
-        return count;
-    }
-    
+  let count = 0;
+  try {
+    let result = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${userId}`
+    );
+    count = result.data.body.length;
+  } catch (error) {
+    console.log(error);
+  }
+  return count;
+};
 
 //ex1
 const getUTCDateTime = () => {
-    return new Promise((resolve) => {
-        let currentday = new Date()
-        let current = currentday.toUTCString();
-        resolve(current)
-    });
-}
-    
-
+  return new Promise((resolve) => {
+    let currentday = new Date();
+    let current = currentday.toUTCString();
+    resolve(current);
+  });
+};
 
 const main = async () => {
-    ex2();
-}
+  ex2();
+};
 
 main();
