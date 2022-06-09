a1f907925fc99ee8977cfd927233b5242cfd04c1
Joel Sipin <jsipin@xpanxion.com>
2022-06-09 12:17:12 -0400

    Ex4: getNames

--- students/6/main.js
+++ students/6/main.js
@@ -21,7 +21,8 @@ const ex3 = async () => {
 }
 
 const ex4 = async () => {
-    console.log("TODO...")
+    let names = await getNames();
+    console.log(names);
 }
 
 const ex5 = async () => {
@@ -32,7 +33,27 @@ const ex6 = async () => {
     console.log("TODO...")
 }
 
-const getFirstName = async (userId) => {                      //ex3
+const getNames = async () => {                         //ex4
+    let array = [];
+    try {
+        let result = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+        let names = result.data
+        for (i = 0; i < names.length; i++) {
+            array.push(names[i].name)
+        }
+
+        return array.sort();
+    }
+
+    catch (err) {                                           //Promise rejection
+        console.log(err);
+    }
+}
+
+
+
+
+const getFirstName = async (userId) => {                         //ex3
     try {
         let result = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
         let name = result.data.name
@@ -43,29 +64,29 @@ const getFirstName = async (userId) => {                      //ex3
     }
 }
 
-    const countCharsInBody = async (userId) => {            //ex2
-        let count = 0;
-        try {
-            let result = await axios.get(`https://jsonplaceholder.typicode.com/posts/${userId}`);
-            count = result.data.body.length
-        }
-        catch (err) {                                        //Promise rejection
-            console.log(err);
-        }
-        return count;
-
+const countCharsInBody = async (userId) => {                //ex2
+    let count = 0;
+    try {
+        let result = await axios.get(`https://jsonplaceholder.typicode.com/posts/${userId}`);
+        count = result.data.body.length
     }
-
-
-    const getUTCDateTime = () => {                           //ex1
-        return new Promise((resolve) => {
-            let current = new Date()
-            resolve(current.toUTCString())
-        });
+    catch (err) {                                        //Promise rejection
+        console.log(err);
     }
+    return count;
 
-    const main = async () => {
-        ex3();
-    }
+}
 
-    main()
+
+const getUTCDateTime = () => {                              //ex1
+    return new Promise((resolve) => {
+        let current = new Date()
+        resolve(current.toUTCString())
+    });
+}
+
+const main = async () => {
+    ex4();
+}
+
+main()
