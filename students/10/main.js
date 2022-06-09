18e44c4e9fd1a5cd4d4cff27c3dd160af2a1475f
Sherika Fayson <sfayson@xpanxion.com>
2022-06-09 14:07:55 -0400

    Ex5: getNamesShortZip

--- students/10/main.js
+++ students/10/main.js
@@ -26,7 +26,8 @@ const ex4 = async () => {
 };
 
 const ex5 = async () => {
-  console.log("TODO...");
+  let names = await getNamesShortZip();
+  console.log(names);
 };
 
 const ex6 = async () => {
@@ -36,6 +37,23 @@ const ex6 = async () => {
 //
 // Your functions here...
 //
+
+//ex5
+const getNamesShortZip = async () => {
+  let namesArray = [];
+  try {
+    let result = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+    let name = result.data;
+    for (let i = 0; i < name.length; i++) {
+      if (name[i].address.zipcode.length < 6) {
+        namesArray.push(name[i].name);
+      }
+    }
+    return namesArray;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //ex4
 const getNames = async () => {
@@ -87,7 +105,7 @@ const getUTCDateTime = () => {
 };
 
 const main = async () => {
-  ex4();
+  ex5();
 };
 
 main();
